093d013898886fa8eb8754ad6cd5b24aacedb8f7
Mohan <1063022109@qq.com>
2018-10-17 23:08:49 +0800

    port

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-29 17:25:55 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-09-30 18:14:51
+ * @Last Modified time: 2018-10-17 23:08:28
  */
 
  const config = {
-    port     : '9000',
+    port     : '80',
     staticDirectory : './dist' 
     
  }
